ec6cdea8d55b27407dbefb02a96f73e309cdb25b
Fahim Ahmed Patwary <www.fahimahmed2727@gmail.com>
2023-07-10 05:08:39 +0600

    Initial commit

---
+++ app/src/main/java/com/example/shotsense/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.shotsense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        b1=(Button)findViewById(R.id.loginButton);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Logged in",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, Menu.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
