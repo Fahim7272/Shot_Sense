998f8d5c1c6fa41eb40b6feff5f768389aad73f2
Fahim Ahmed Patwary <www.fahimahmed2727@gmail.com>
2023-07-10 22:37:00 +0600

    Straight Drive and the tutorials

--- app/src/main/java/com/example/shotsense/Menu.java
+++ app/src/main/java/com/example/shotsense/Menu.java
@@ -2,14 +2,54 @@ package com.example.shotsense;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Menu extends AppCompatActivity {
 
+    Button cover_drive;
+    Button straight_drive;
+    Button on_drive;
+    Button cut_shot;
+    Button hook_shot;
+    Button flick_shot;
+    Button pull_shot;
+    Button sweep_shot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        cover_drive=(Button)findViewById(R.id.cover_drive_bt);
+        cover_drive.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Cover Drive",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Menu.this, Cover_Drive_Doc.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+        straight_drive=(Button)findViewById(R.id.straight_drive_bt);
+        straight_drive.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Straight Drive",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Menu.this, Straight_Drive_Doc.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
 
 
     }
---
+++ app/src/main/java/com/example/shotsense/Straight_Drive_Doc.java
@@ -0,0 +1,31 @@
+package com.example.shotsense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Straight_Drive_Doc extends AppCompatActivity {
+
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_straight_drive_doc);
+
+
+        b1=(Button)findViewById(R.id.bt1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"COVER DRIVE TUTORIAL",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Straight_Drive_Doc.this, Straight_drive_tutorial.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
