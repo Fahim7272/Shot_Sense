3cb0286acf8e49d5ba9066a52047bd493b822be7
Fahim Ahmed Patwary <www.fahimahmed2727@gmail.com>
2023-07-10 21:38:50 +0600

    Cover Drive all complete

--- app/src/main/java/com/example/shotsense/Cover_drive_tutorial.java
+++ app/src/main/java/com/example/shotsense/Cover_drive_tutorial.java
@@ -2,13 +2,28 @@ package com.example.shotsense;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Cover_drive_tutorial extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cover_drive_tutorial);
+
+        VideoView videoView = findViewById(R.id.cover_drive_tutorial);
+        String videopath = "android.resource://" + getPackageName() + "/" + R.raw.cover_drive;
+        Uri uri = Uri.parse(videopath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+        videoView.start();
     }
 }
