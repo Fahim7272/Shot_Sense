42a30db5865b302456ae5e3f8ea11caa3cc44d31
Fahim Ahmed Patwary <www.fahimahmed2727@gmail.com>
2023-07-10 22:22:48 +0600

    MENU

--- app/src/main/java/com/example/shotsense/Menu.java
+++ app/src/main/java/com/example/shotsense/Menu.java
@@ -9,7 +9,7 @@ public class Menu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cover_drive_doc);
+        setContentView(R.layout.activity_menu);
 
 
     }
